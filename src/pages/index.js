bb428869b2c07650699dcf5d11a45cb62494412a
Mark Casias <markie@kanopistudios.om>
2021-04-07 23:08:29 -0600

    syntax

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,10 +11,10 @@ import '../scss/main.scss';
 const IndexPage = () => {
   return (
     <main>
-      <Hero title="New Mexico Rugby" tagline="New Mexico's only youth rugby organization" />
+      <Hero title="Rugby New Mexico" tagline="New Mexico's only youth rugby organization" />
       <Menu />
       <main className="container px-4 mx-auto md:px-0">
-        <p>New Mexico Rugby is a registered non-profit and all-volunteer sports organization, dedicated to the development of young athletes in New Mexico and west Texas through youth and high school rugby for all. Our programs, leagues, and clubs state-wide offer contact and non-contact rugby open to all elementary, middle school, and high school student-athletes. NMYR's mission is to develop and grow the sport of rugby for all youth within the state of New Mexico. Our goals are to provide a safe and fair sporting environment, and to develop youth character by introducing the values of rugby into a team sport experience that will endure for a lifetime."</p>
+        <p>Rugby New Mexico is a registered non-profit and all-volunteer sports organization, dedicated to the development of young athletes in New Mexico and west Texas through youth and high school rugby for all. Our programs, leagues, and clubs state-wide offer contact and non-contact rugby open to all elementary, middle school, and high school student-athletes. NMYR's mission is to develop and grow the sport of rugby for all youth within the state of New Mexico. Our goals are to provide a safe and fair sporting environment, and to develop youth character by introducing the values of rugby into a team sport experience that will endure for a lifetime."</p>
 
         <h2>About Rugby</h2>
         <p>"Rugby is one of the fastest growing sports in the United States. Many people are familiar with rugby as a fringe sport, but you may be surprised to learn that it is actually the second most popular sport in the world behind soccer. In fact, in Europe, Australia and South Africa, the teams and players are elevated to a star status that exceeds that of NFL players in the United States! Many teams fill 80,000 seat stadiums for international match play.</p>
